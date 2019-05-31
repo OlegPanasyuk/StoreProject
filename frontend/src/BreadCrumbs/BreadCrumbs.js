0ab58ab5ad2bfdbf34fdf1e7427383e8f890eba9
Pahmurny-itechart <Siarhei.Pakhmurny@itechart-group.com>
2019-05-31 16:57:42 +0300

    Add eslint rules, refactor component

--- frontend/src/BreadCrumbs/BreadCrumbs.js
+++ frontend/src/BreadCrumbs/BreadCrumbs.js
@@ -1,39 +1,32 @@
-import React, { Component } from 'react';
-import { Breadcrumb } from 'react-bootstrap';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { Breadcrumb } from 'react-bootstrap';
 
-class BreadCrumbs extends Component {
-    render() {
-        const { obj, handleClick } = this.props;
-        let arrItems = obj.map((element, i) => {
-            let elem = null;
-            if (i === (obj.length - 1)) {
-                elem = (
-                    <Breadcrumb.Item href="#" onClick={() => handleClick(element.id)} key={element.id} active>
-                        {element.name}
-                    </Breadcrumb.Item>
-                );
-            } else {
-                elem = (
-                    <Breadcrumb.Item href="#" onClick={() => handleClick(element.id)} key={element.id} >
-                        {element.name}
-                    </Breadcrumb.Item>
-                );
-            }
-            return elem;
-        });
+const BreadCrumbs = ({ obj, handleClick }) => {
+  const breadcrumbItems = obj.map((element, i) => (
+    <Breadcrumb.Item
+      href="#" onClick={() => handleClick(element.id)} key={element.id}
+      active={i === (obj.length - 1)}
+    >
+      {element.name}
+    </Breadcrumb.Item>
+  ));
 
-        return (
-            <Breadcrumb variant="secondary" >
-                {arrItems}
-            </Breadcrumb>
-        );
-    }
-}
+  return (
+    <Breadcrumb variant="secondary">
+      {breadcrumbItems}
+    </Breadcrumb>
+  );
+};
 
 BreadCrumbs.propTypes = {
-    obj: PropTypes.array,
-    handleClick: PropTypes.func
+  obj: PropTypes.array,
+  handleClick: PropTypes.func,
+};
+
+BreadCrumbs.defaultProps = {
+  obj: [],
+  handleClick: () => null,
 };
 
 export default BreadCrumbs;
